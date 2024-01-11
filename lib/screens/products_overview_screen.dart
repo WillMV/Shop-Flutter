ff7595d1724b7a9eccdd9a31f958b9dd962b4443
Willian M Vargas <willianm.vargas@gmail.com>
2024-01-11 12:34:08 -0300

    feat: Create product_overview_screen feat: Add primary and secondary colors to ThemeData.ColorScheme

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/data/dummy_products.dart';
+import 'package:shopping/screens/product_detail_screen.dart';
 
-class ProductsOverviewScreen extends StatefulWidget {
+class ProductsOverviewScreen extends StatelessWidget {
   const ProductsOverviewScreen({super.key});
 
   @override
-  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
-}
-
-class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Shopping'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 15,
+          childAspectRatio: 1,
+          mainAxisSpacing: 15,
+        ),
+        padding: const EdgeInsets.all(15),
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = products[index];
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: GridTile(
+              footer: GridTileBar(
+                backgroundColor: Colors.black87,
+                title: Text(
+                  item.title,
+                  textAlign: TextAlign.center,
+                ),
+                trailing: const Icon(Icons.shopping_cart),
+                leading: Icon(
+                  Icons.favorite_border,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ProductDetailScreen()));
+                },
+                child: Image.network(
+                  item.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
+
+final products = dummyProducts;
